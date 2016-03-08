214b9a94af79bcee0c67c91dce3ae315bc2d77bf
chenxiaosong <smallhorse87@163.com>
2016-03-08 22:01:36 +0800

    调整了一下文件内函数位置

--- app/src/main/java/com/zhitian/mybole/activity/merchant/MerchantFormActivity.java
+++ app/src/main/java/com/zhitian/mybole/activity/merchant/MerchantFormActivity.java
@@ -21,6 +21,7 @@ import android.widget.Toast;
 import com.yalantis.ucrop.UCrop;
 import com.yalantis.ucrop.UCropActivity;
 import com.zhitian.mybole.R;
+import com.zhitian.mybole.base.BaseActivity;
 
 import java.io.File;
 
@@ -28,14 +29,11 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MerchantFormActivity extends AppCompatActivity {
+public class MerchantFormActivity extends BaseActivity {
     private static final String TAG = "SampleActivity";
 
     private static final int REQUEST_SELECT_PICTURE = 0x01;
     private static final String SAMPLE_CROPPED_IMAGE_NAME = "SampleCropImage.jpeg";
-
-    protected static final int REQUEST_STORAGE_READ_ACCESS_PERMISSION = 101;
-    protected static final int REQUEST_STORAGE_WRITE_ACCESS_PERMISSION = 102;
 
     @Bind(R.id.ll_logo)
     LinearLayout mllLogo;
@@ -44,7 +42,7 @@ public class MerchantFormActivity extends AppCompatActivity {
 
     private Uri mDestinationUri;
 
-    private AlertDialog mAlertDialog;
+
 
 
     @Override
@@ -55,6 +53,27 @@ public class MerchantFormActivity extends AppCompatActivity {
 
         mDestinationUri = Uri.fromFile(new File(getCacheDir(), SAMPLE_CROPPED_IMAGE_NAME));
 
+    }
+
+    @OnClick(R.id.ll_logo)
+    public void onClick() {
+        pickFromGallery();
+    }
+
+    private void pickFromGallery() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN // Permission was added in API Level 16
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermission(Manifest.permission.READ_EXTERNAL_STORAGE,
+                    getString(R.string.permission_read_storage_rationale),
+                    REQUEST_STORAGE_READ_ACCESS_PERMISSION);
+        } else {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            startActivityForResult(Intent.createChooser(intent, getString(R.string.label_select_picture)), REQUEST_SELECT_PICTURE);
+        }
     }
 
     @Override
@@ -73,38 +92,6 @@ public class MerchantFormActivity extends AppCompatActivity {
         }
         if (resultCode == UCrop.RESULT_ERROR) {
             handleCropError(data);
-        }
-    }
-
-    /**
-     * Callback received when a permissions request has been completed.
-     */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_STORAGE_READ_ACCESS_PERMISSION:
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    pickFromGallery();
-                }
-                break;
-            default:
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
-
-    private void pickFromGallery() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN // Permission was added in API Level 16
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-            requestPermission(Manifest.permission.READ_EXTERNAL_STORAGE,
-                    getString(R.string.permission_read_storage_rationale),
-                    REQUEST_STORAGE_READ_ACCESS_PERMISSION);
-        } else {
-            Intent intent = new Intent();
-            intent.setType("image/*");
-            intent.setAction(Intent.ACTION_GET_CONTENT);
-            intent.addCategory(Intent.CATEGORY_OPENABLE);
-            startActivityForResult(Intent.createChooser(intent, getString(R.string.label_select_picture)), REQUEST_SELECT_PICTURE);
         }
     }
 
@@ -143,52 +130,5 @@ public class MerchantFormActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * Requests given permission.
-     * If the permission has been denied previously, a Dialog will prompt the user to grant the
-     * permission, otherwise it is requested directly.
-     */
-    protected void requestPermission(final String permission, String rationale, final int requestCode) {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
-            showAlertDialog(getString(R.string.permission_title_rationale), rationale,
-                    new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            ActivityCompat.requestPermissions(MerchantFormActivity.this,
-                                    new String[]{permission}, requestCode);
-                        }
-                    }, getString(R.string.label_ok), null, getString(R.string.label_cancel));
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{permission}, requestCode);
-        }
-    }
 
-    /**
-     * This method shows dialog with given title & message.
-     * Also there is an option to pass onClickListener for positive & negative button.
-     *
-     * @param title                         - dialog title
-     * @param message                       - dialog message
-     * @param onPositiveButtonClickListener - listener for positive button
-     * @param positiveText                  - positive button text
-     * @param onNegativeButtonClickListener - listener for negative button
-     * @param negativeText                  - negative button text
-     */
-    protected void showAlertDialog(@Nullable String title, @Nullable String message,
-                                   @Nullable DialogInterface.OnClickListener onPositiveButtonClickListener,
-                                   @NonNull String positiveText,
-                                   @Nullable DialogInterface.OnClickListener onNegativeButtonClickListener,
-                                   @NonNull String negativeText) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(title);
-        builder.setMessage(message);
-        builder.setPositiveButton(positiveText, onPositiveButtonClickListener);
-        builder.setNegativeButton(negativeText, onNegativeButtonClickListener);
-        mAlertDialog = builder.show();
-    }
-
-    @OnClick(R.id.ll_logo)
-    public void onClick() {
-        pickFromGallery();
-    }
 }
