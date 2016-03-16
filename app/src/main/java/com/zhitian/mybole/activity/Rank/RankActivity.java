ea1102ba0d318ef94e27be4ab8a49b033e337223
chenxiaosong <smallhorse87@163.com>
2016-03-16 15:27:52 +0800

    排名页面布局

---
+++ app/src/main/java/com/zhitian/mybole/activity/Rank/RankActivity.java
@@ -0,0 +1,15 @@
+package com.zhitian.mybole.activity.Rank;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zhitian.mybole.R;
+
+public class RankActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rank);
+    }
+}
